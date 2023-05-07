46acf4968597a86a7944d0b4dbf63c9b7da1db09
vascabarkapa <vascabarkapa@gmail.com>
2023-05-08 00:33:07 +0200

    implemented react-navigation

---
+++ navigation/SmartNavigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Dashboard from '../screens/Dashboard';
+import Temperature from '../screens/Temperature';
+import Humidity from '../screens/Humidity';
+
+const Stack = createStackNavigator();
+
+export default function SmartNavigation() {
+    return (
+        <Stack.Navigator initialRouteName="Dashboard">
+            <Stack.Screen name="Dashboard" component={Dashboard} />
+            <Stack.Screen name="Temperature" component={Temperature} />
+            <Stack.Screen name="Humidity" component={Humidity} />
+        </Stack.Navigator>
+    );
+}
