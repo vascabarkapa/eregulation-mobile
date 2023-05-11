8dad88fd8647f61978ccf1abb8676db4e3abb289
vascabarkapa <vascabarkapa@gmail.com>
2023-05-11 23:54:24 +0200

    disabled header options for dashboard

--- navigation/SmartNavigation.js
+++ navigation/SmartNavigation.js
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 export default function SmartNavigation() {
     return (
         <Stack.Navigator initialRouteName="Dashboard">
-            <Stack.Screen name="Dashboard" component={Dashboard} />
+            <Stack.Screen name="Dashboard" component={Dashboard} options={{ headerShown: false }} />
             <Stack.Screen name="Temperature" component={Temperature} />
             <Stack.Screen name="Humidity" component={Humidity} />
         </Stack.Navigator>
