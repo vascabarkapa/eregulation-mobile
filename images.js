1a650f444fcf30e96eb0ac86ff029fc9ebc326b3
vascabarkapa <vascabarkapa@gmail.com>
2023-05-13 14:26:07 +0200

    export images

---
+++ images.js
@@ -0,0 +1,15 @@
+export const adaptiveIcon = require('./assets/adaptive-icon.png');
+export const mainLogo = require('./assets/main-logo.png');
+export const lightLogo = require('./assets/light-logo.png');
+export const favicon = require('./assets/favicon.png');
+export const icon = require('./assets/icon.png');
+export const splash = require('./assets/splash.png');
+
+export default {
+    adaptiveIcon,
+    mainLogo,
+    lightLogo,
+    favicon,
+    icon,
+    splash
+}
