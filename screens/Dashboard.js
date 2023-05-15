872bc8a9858d12c72373906b22677a8036f5fa3e
vascabarkapa <vascabarkapa@gmail.com>
2023-05-15 23:26:45 +0200

    added background image on app

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Image, StatusBar, StyleSheet, TouchableOpacity, View, Linking } from 'react-native';
+import { ImageBackground, Image, StatusBar, StyleSheet, TouchableOpacity, View, Linking } from 'react-native';
 
 import * as theme from '../styles';
 import * as images from '../images';
@@ -21,102 +21,107 @@ class Dashboard extends Component {
 
         return (
             <>
-                <StatusBar backgroundColor="#d9b9c3" />
-                <Block style={styles.dashboard}>
-                    <Block center>
-                        <Image source={images.mainLogo} style={styles.image} />
-                        <Text weight='600' spacing={0.1}>Take control of your surroundings with eRegulation.</Text>
-                        <Text weight='600' spacing={0.1}>Your smart solution for sensor regulation.</Text>
-                    </Block>
-
-                    <Block column style={{ marginVertical: theme.sizes.base * 2, }}>
-                        <Text welcome>Good afternoon</Text>
-                        <Text name>Test User</Text>
-                    </Block>
-
-                    <Block row style={{ paddingVertical: 10 }}>
-                        <Block flex={2} row style={{ alignItems: 'flex-end', }}>
-                            <Text h1>28</Text>
-                            <Text h1 size={34} height={80} weight='600' spacing={0.1}>°C</Text>
-                        </Block>
-                        <Block flex={1.5} row style={{ alignItems: 'flex-end', }}>
-                            <Text h1>48</Text>
-                            <Text h1 size={34} height={80} weight='600' spacing={0.1}>%</Text>
-                        </Block>
-                    </Block>
-
-                    <View contentContainerStyle={styles.buttons} showsVerticalScrollIndicator={false}>
-                        <Block column space="between">
-                            <Block row space="around" style={{ marginVertical: theme.sizes.base }}>
-                                <TouchableOpacity
-                                    activeOpacity={0.8}
-                                    onPress={() => navigation.navigate('Temperature', { name: 'temperature' })}
-                                >
-                                    <Block center middle style={styles.button}>
-                                        <TemperatureIcon size={38} />
-                                        <Text
-                                            button
-                                            color={'background'}
-                                            style={{ marginTop: theme.sizes.base * 0.5 }}
-                                        >
-                                            {settings['temperature'].name}
-                                        </Text>
-                                    </Block>
-                                </TouchableOpacity>
-
-                                <TouchableOpacity
-                                    activeOpacity={0.8}
-                                    onPress={() => navigation.navigate('Humidity', { name: 'humidity' })}
-                                >
-                                    <Block center middle style={styles.button}>
-                                        <HumidityIcon size={38} />
-                                        <Text
-                                            button
-                                            color={'background'}
-                                            style={{ marginTop: theme.sizes.base * 0.5 }}
-                                        >
-                                            {settings['humidity'].name}
-                                        </Text>
-                                    </Block>
-                                </TouchableOpacity>
+                <Block style={styles.container}>
+                    <ImageBackground
+                        source={images.backgroundOpacity15}
+                        style={styles.backgroundImage}
+                    >
+                        <StatusBar translucent={true} backgroundColor="transparent" />
+                        <Block style={styles.dashboard}>
+                            <Block center>
+                                <Image source={images.mainLogo} style={styles.logo} />
                             </Block>
 
-                            <Block row space="around" style={{ marginVertical: theme.sizes.base }}>
-                                <TouchableOpacity
-                                    activeOpacity={0.8}
-                                    onPress={redirectToStatistics}
-                                >
-                                    <Block center middle style={styles.button}>
-                                        <StatisticsIcon size={38} />
-                                        <Text
-                                            button
-                                            color={'background'}
-                                            style={{ marginTop: theme.sizes.base * 0.5 }}
-                                        >
-                                            {settings['statistics'].name}
-                                        </Text>
-                                    </Block>
-                                </TouchableOpacity>
-
-                                <TouchableOpacity
-                                    activeOpacity={0.8}
-                                    onPress={() => navigation.navigate('Settings', { name: 'settings' })}
-                                >
-                                    <Block center middle style={styles.button}>
-                                        <SettingsIcon size={38} />
-                                        <Text
-                                            button
-                                            color={'background'}
-                                            style={{ marginTop: theme.sizes.base * 0.5 }}
-                                        >
-                                            {settings['settings'].name}
-                                        </Text>
-                                    </Block>
-                                </TouchableOpacity>
+                            <Block center column style={{ marginVertical: theme.sizes.base * 2, }}>
+                                <Text welcome>Good afternoon</Text>
+                                <Text name>Test User</Text>
                             </Block>
+
+                            <Block row style={{ paddingVertical: 10 }}>
+                                <Block flex={2} row style={{ alignItems: 'flex-end', }}>
+                                    <Text h1>28</Text>
+                                    <Text h1 size={34} height={80} weight='600' spacing={0.1}>°C</Text>
+                                </Block>
+                                <Block flex={1.5} row style={{ alignItems: 'flex-end', }}>
+                                    <Text h1>48</Text>
+                                    <Text h1 size={34} height={80} weight='600' spacing={0.1}>%</Text>
+                                </Block>
+                            </Block>
+
+                            <View contentContainerStyle={styles.buttons} showsVerticalScrollIndicator={false}>
+                                <Block column space="between">
+                                    <Block row space="around" style={{ marginVertical: theme.sizes.base }}>
+                                        <TouchableOpacity
+                                            activeOpacity={0.8}
+                                            onPress={() => navigation.navigate('Temperature', { name: 'temperature' })}
+                                        >
+                                            <Block center middle style={styles.button}>
+                                                <TemperatureIcon size={38} />
+                                                <Text
+                                                    button
+                                                    color={'background'}
+                                                    style={{ marginTop: theme.sizes.base * 0.5 }}
+                                                >
+                                                    {settings['temperature'].name}
+                                                </Text>
+                                            </Block>
+                                        </TouchableOpacity>
+
+                                        <TouchableOpacity
+                                            activeOpacity={0.8}
+                                            onPress={() => navigation.navigate('Humidity', { name: 'humidity' })}
+                                        >
+                                            <Block center middle style={styles.button}>
+                                                <HumidityIcon size={38} />
+                                                <Text
+                                                    button
+                                                    color={'background'}
+                                                    style={{ marginTop: theme.sizes.base * 0.5 }}
+                                                >
+                                                    {settings['humidity'].name}
+                                                </Text>
+                                            </Block>
+                                        </TouchableOpacity>
+                                    </Block>
+
+                                    <Block row space="around" style={{ marginVertical: theme.sizes.base }}>
+                                        <TouchableOpacity
+                                            activeOpacity={0.8}
+                                            onPress={redirectToStatistics}
+                                        >
+                                            <Block center middle style={styles.button}>
+                                                <StatisticsIcon size={38} />
+                                                <Text
+                                                    button
+                                                    color={'background'}
+                                                    style={{ marginTop: theme.sizes.base * 0.5 }}
+                                                >
+                                                    {settings['statistics'].name}
+                                                </Text>
+                                            </Block>
+                                        </TouchableOpacity>
+
+                                        <TouchableOpacity
+                                            activeOpacity={0.8}
+                                            onPress={() => navigation.navigate('Settings', { name: 'settings' })}
+                                        >
+                                            <Block center middle style={styles.button}>
+                                                <SettingsIcon size={38} />
+                                                <Text
+                                                    button
+                                                    color={'background'}
+                                                    style={{ marginTop: theme.sizes.base * 0.5 }}
+                                                >
+                                                    {settings['settings'].name}
+                                                </Text>
+                                            </Block>
+                                        </TouchableOpacity>
+                                    </Block>
+                                </Block>
+                            </View>
                         </Block>
-                    </View>
-                </Block>
+                    </ImageBackground>
+                </Block >
             </>
         )
     }
@@ -133,9 +138,9 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: theme.sizes.base * 2,
         marginBottom: -theme.sizes.base * 6,
-        backgroundColor: theme.colors.background
+        marginTop: theme.sizes.base * 3,
     },
-    image: {
+    logo: {
         marginTop: theme.sizes.base,
         marginBottom: theme.sizes.base * 2,
         width: '100%',
@@ -151,5 +156,12 @@ const styles = StyleSheet.create({
         width: 151,
         height: 151,
         borderRadius: 150 / 10,
-    }
+    },
+    container: {
+        flex: 1,
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover', // Adjust the image resizing mode as needed
+    },
 })
