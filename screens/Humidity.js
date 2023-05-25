51158daa9d80b36d967bc92ee19139272c0b50c3
vascabarkapa <vascabarkapa@gmail.com>
2023-05-26 00:53:23 +0200

    added sliders

--- screens/Humidity.js
+++ screens/Humidity.js
@@ -9,8 +9,20 @@ import {
 import * as theme from '../styles';
 import * as images from '../images';
 import { Block, Text } from '../components';
+import Slider from '@react-native-community/slider';
 
 const Humidity = ({ navigation, settings }) => {
+    const [minHumidity, setMinHumidity] = useState(20);
+    const [maxHumidity, setMaxHumidity] = useState(40);
+
+    const handleMinHumiditySliderChange = (value) => {
+        setMinHumidity(Math.floor(value));
+    };
+
+    const handleMaxHumiditySliderChange = (value) => {
+        setMaxHumidity(Math.floor(value));
+    };
+
     return (
         <KeyboardAvoidingView
             behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -23,6 +35,42 @@ const Humidity = ({ navigation, settings }) => {
                     <Block style={styles.humidityPage}>
                         <Block center>
                             <Text h2 bold>Humidity</Text>
+                        </Block>
+                        <Block flex={1} style={{ paddingTop: theme.sizes.base * 2 }}>
+                            <Block column style={{ marginVertical: theme.sizes.base * 2 }}>
+                                <Block row space="between">
+                                    <Text welcome color="black">MIN</Text>
+                                    <Text welcome bold color="black">{minHumidity}%</Text>
+                                </Block>
+                                <Slider
+                                    value={minHumidity}
+                                    minimumValue={0}
+                                    maximumValue={100}
+                                    thumbTintColor={theme.colors.button}
+                                    minimumTrackTintColor={theme.colors.button}
+                                    maximumTrackTintColor={theme.colors.gray2}
+                                    onValueChange={handleMinHumiditySliderChange}
+                                    upperLimit={maxHumidity - 1}
+                                    step={1}
+                                />
+                            </Block>
+                            <Block column style={{ marginVertical: theme.sizes.base * 2 }}>
+                                <Block row space="between">
+                                    <Text welcome color="black">MAX</Text>
+                                    <Text welcome bold color="black">{maxHumidity}%</Text>
+                                </Block>
+                                <Slider
+                                    value={maxHumidity}
+                                    minimumValue={0}
+                                    maximumValue={100}
+                                    thumbTintColor={theme.colors.button}
+                                    minimumTrackTintColor={theme.colors.button}
+                                    maximumTrackTintColor={theme.colors.gray2}
+                                    onValueChange={handleMaxHumiditySliderChange}
+                                    lowerLimit={minHumidity + 1}
+                                    step={1}
+                                />
+                            </Block>
                         </Block>
                     </Block>
                 </ImageBackground>
--- screens/Temperature.js
+++ screens/Temperature.js
@@ -9,8 +9,20 @@ import {
 import * as theme from '../styles';
 import * as images from '../images';
 import { Block, Text } from '../components';
+import Slider from '@react-native-community/slider';
 
 const Temperature = ({ navigation, settings }) => {
+    const [minTemperature, setMinTemperature] = useState(18);
+    const [maxTemperature, setMaxTemperature] = useState(24);
+
+    const handleMinTemperatureSliderChange = (value) => {
+        setMinTemperature(Math.floor(value));
+    };
+
+    const handleMaxTemperatureSliderChange = (value) => {
+        setMaxTemperature(Math.floor(value));
+    };
+
     return (
         <KeyboardAvoidingView
             behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -23,6 +35,42 @@ const Temperature = ({ navigation, settings }) => {
                     <Block style={styles.temperaturePage}>
                         <Block center>
                             <Text h2 bold>Temperature</Text>
+                        </Block>
+                        <Block flex={1} style={{ paddingTop: theme.sizes.base * 2 }}>
+                            <Block column style={{ marginVertical: theme.sizes.base * 2 }}>
+                                <Block row space="between">
+                                    <Text welcome color="black">MIN</Text>
+                                    <Text welcome bold color="black">{minTemperature}°C</Text>
+                                </Block>
+                                <Slider
+                                    value={minTemperature}
+                                    minimumValue={-16}
+                                    maximumValue={36}
+                                    thumbTintColor={theme.colors.button}
+                                    minimumTrackTintColor={theme.colors.button}
+                                    maximumTrackTintColor={theme.colors.gray2}
+                                    onValueChange={handleMinTemperatureSliderChange}
+                                    upperLimit={maxTemperature - 1}
+                                    step={1}
+                                />
+                            </Block>
+                            <Block column style={{ marginVertical: theme.sizes.base * 2 }}>
+                                <Block row space="between">
+                                    <Text welcome color="black">MAX</Text>
+                                    <Text welcome bold color="black">{maxTemperature}°C</Text>
+                                </Block>
+                                <Slider
+                                    value={maxTemperature}
+                                    minimumValue={-16}
+                                    maximumValue={36}
+                                    thumbTintColor={theme.colors.button}
+                                    minimumTrackTintColor={theme.colors.button}
+                                    maximumTrackTintColor={theme.colors.gray2}
+                                    onValueChange={handleMaxTemperatureSliderChange}
+                                    lowerLimit={minTemperature + 1}
+                                    step={1}
+                                />
+                            </Block>
                         </Block>
                     </Block>
                 </ImageBackground>
