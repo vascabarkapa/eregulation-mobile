9b8bcc92005ee944d3f3f75d2f2f371542c1a312
vascabarkapa <vascabarkapa@gmail.com>
2023-06-25 16:44:10 +0200

    global variables

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { DarkTheme, NavigationContainer } from '@react-navigation/native';
 
 import SmartNavigation from './navigation/SmartNavigation';
+import { GlobalProvider } from './contexts/GlobalContext';
 
 export default function App() {
   return (
-    <NavigationContainer theme={DarkTheme}>
-      <SmartNavigation />
-    </NavigationContainer>
+    <GlobalProvider>
+      <NavigationContainer theme={DarkTheme}>
+        <SmartNavigation />
+      </NavigationContainer>
+    </GlobalProvider>
   );
 }
