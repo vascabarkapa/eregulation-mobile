5de4d1a8102d7f6d7975507c233aa330e5061814
vascabarkapa <vascabarkapa@gmail.com>
2023-06-25 20:25:26 +0200

    added toastr

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { DarkTheme, NavigationContainer } from '@react-navigation/native';
-
+import { RootSiblingParent } from 'react-native-root-siblings';
 import SmartNavigation from './navigation/SmartNavigation';
 import { GlobalProvider } from './contexts/GlobalContext';
 
 export default function App() {
   return (
-    <GlobalProvider>
-      <NavigationContainer theme={DarkTheme}>
-        <SmartNavigation />
-      </NavigationContainer>
-    </GlobalProvider>
+    <RootSiblingParent>
+      <GlobalProvider>
+        <NavigationContainer theme={DarkTheme}>
+          <SmartNavigation />
+        </NavigationContainer>
+      </GlobalProvider>
+    </RootSiblingParent>
   );
 }
