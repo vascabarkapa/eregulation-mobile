df02b4d4570c00ac4279ad9864b1b7e4cd4c32d7
vascabarkapa <vascabarkapa@gmail.com>
2023-06-25 18:02:37 +0200

    dashboard ping

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -86,6 +86,7 @@ const Dashboard = ({ navigation, settings }) => {
             () => {
                 console.log('MQTT connected');
                 MqttService.subscribe('eregulation', onMessageArrived);
+                MqttService.send('eregulation', 'ping');
             },
             (error) => {
                 console.error('MQTT connection failed:', error);
@@ -107,11 +108,6 @@ const Dashboard = ({ navigation, settings }) => {
             setLiveTemperature(parsedLiveData.temperature);
             setLiveHumidity(parsedLiveData.humidity);
         }
-    };
-
-    const sendMessage = () => {
-        const message = 'Hello, MQTT!';
-        MqttService.send('eregulation', message);
     };
 
     return (
