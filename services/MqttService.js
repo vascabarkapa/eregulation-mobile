28cf3983decb0c529e663a42bde93b3c018144d4
Vasilije Čabarkapa <77989115+vascabarkapa@users.noreply.github.com>
2024-12-05 22:45:59 +0100

    removed unused comments

--- services/MqttService.js
+++ services/MqttService.js
@@ -2,22 +2,22 @@ import { Client, Message } from 'paho-mqtt';
 
 class MqttService {
   constructor() {
-    this.clientId = 'clientId' + Math.random().toString(16).substr(2, 8); // Generate a random client ID
-    this.userName = 'eregulation'; // Replace with your MQTT username
-    this.password = 'eRegulation123!'; // Replace with your MQTT password
+    this.clientId = 'clientId' + Math.random().toString(16).substr(2, 8);
+    this.userName = 'eregulation';
+    this.password = 'eRegulation123!';
 
     this.client = new Client('e3a0ad2ab69842fbb684a383f2306b47.s2.eu.hivemq.cloud', 8884, 'clientId' + Math.random(1, 1000));
-    this.isConnected = false; // Flag to track connection status
+    this.isConnected = false;
   }
 
   connect(onConnect, onFailure) {
     this.client.connect({
       onSuccess: () => {
-        this.isConnected = true; // Set the flag to true on successful connection
+        this.isConnected = true;
         onConnect();
       },
       onFailure: (error) => {
-        this.isConnected = false; // Set the flag to false on connection failure
+        this.isConnected = false;
         onFailure(error);
       },
       useSSL: true,
